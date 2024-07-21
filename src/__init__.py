3b3b2c53230c71bea9801de295723ec15158ee94
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-07-21 02:48:19 +0200

    Refactor config and static folder (production folder, also in src)

--- src/__init__.py
+++ src/__init__.py
@@ -5,9 +5,9 @@ PROJECT_PATH = Path(__file__).parents[1]
 SOURCE_PATH = Path(__file__).parents[0]
 
 # Globals variables for this project
-STATIC_PATH = PROJECT_PATH / "static"
+STATIC_PATH = SOURCE_PATH / "static"
 TEMPLATES_PATH = SOURCE_PATH / "templates"
 
 # Configuration paths for the website (content)
-CONFIG_PATH = PROJECT_PATH / "config"  # Contain translations for the website
+CONFIG_PATH = SOURCE_PATH / "config"  # Contain translations for the website
 LOCALES_PATH = CONFIG_PATH / "locales"  # Contain translations for the website
