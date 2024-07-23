93044b0e115df4ce8c8543021ac8f9076da2a510
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-07-23 21:50:49 +0200

    Add SSL config to .env

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,17 @@
 import logging
+import os
 import sys
 from pathlib import Path
 
 import uvicorn
 from dotenv import load_dotenv
 
-from src import ENV_PATH
-
 # Automatically add the working directory
 path = Path(__file__).parents[1].absolute()
 sys.path.append(f"{path}")
 
 from src.app import app  # noqa: E402
+from src import ENV_PATH  # noqa: E402
 
 
 def main():
@@ -33,8 +33,23 @@ def main():
     # Ajoute la commande de l'utilisateur au log
     logging.debug(f"Command use \"{' '.join(sys.argv)}\"")
 
+    # Récupère l'environnement
+    environment = os.getenv("ENVIRONMENT", "development")
+
+    if environment not in ["development", "production"]:
+        raise ValueError(f"Environment '{environment}' not supported")
+
+    logging.debug(f"Environment: '{environment}'")
+
+    host = "127.0.0.1" if environment == "development" else "0.0.0.0"
+    port = 8000 if environment == "development" else 443
+
+    # Récupère la configuration SSL
+    ssl_keyfile = os.getenv("SSL_KEYFILE") if environment == "production" else None
+    ssl_certfile = os.getenv("SSL_CERTFILE") if environment == "production" else None
+
     try:
-        uvicorn.run(app, host="127.0.0.1", port=8000)  # noqa: B104
+        uvicorn.run(app, host=host, port=port, ssl_keyfile=ssl_keyfile, ssl_certfile=ssl_certfile)
     except KeyboardInterrupt as exception:
         logging.debug(f"Exiting the program: '{exception}'")
 
